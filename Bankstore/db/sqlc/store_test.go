2eacb668314998990d5acb6e1670a8826a06e3a8
Vadim Shikhovtsov <5208643@mail.ru>
2024-12-26 11:26:25 +0300

    Add concurrent tests

--- Bankstore/db/sqlc/store_test.go
+++ Bankstore/db/sqlc/store_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestTransferTx(t *testing.T) {
@@ -13,14 +15,42 @@ func TestTransferTx(t *testing.T) {
 	account2 := createRandomAccount(t)
 	fmt.Println("Before:", account1.Balance, account2.Balance)
 
+	n := 5
 	amount := int64(10)
-	result, err := store.TransferTx(context.Background(), TransferTxParams{
-		FromAccountID: account1.ID,
-		ToAccountID: account2.ID,
-		Amount: amount,
-	})
-	if err != nil {
-		fmt.Println(err)
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+	// run n concurrent transfer transactions
+	for i := 0; i < n; i++ {
+		go func() {
+		result, err := store.TransferTx(context.Background(), TransferTxParams{
+			FromAccountID: account1.ID,
+			ToAccountID: account2.ID,
+			Amount: amount,
+		})
+		errs <- err
+		results <- result
+		}()
 	}
-	fmt.Printf("%v", result)
+
+	// Check results
+	for i := 0; i < n; i++ {
+		// check errors
+		err := <- errs
+		require.NoError(t, err)
+
+		// check transfers
+		result := <- results
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+
+	}
 }
