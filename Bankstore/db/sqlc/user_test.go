b74d184059cd503d92f1c08a4223ec8a672bec55
Vadim Shikhovtsov <5208643@mail.ru>
2024-12-27 16:04:29 +0300

    Add user_test

--- Bankstore/db/sqlc/user_test.go
+++ Bankstore/db/sqlc/user_test.go
@@ -12,5 +12,43 @@ import (
 
 func createRandomUser(t *testing.T) User {
 	arg := CreateUserParams(utils.RandomUser())
-	
+
+	hashedPassword, err := utils.HashPassword(arg.HashedPassword)
+	require.NoError(t, err)
+
+	arg.HashedPassword = hashedPassword
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.HashedPassword, user.HashedPassword)
+	require.Equal(t, arg.FullName, user.FullName)
+	require.Equal(t, arg.Email, user.Email)
+	require.NotZero(t, user.CreatedAt)
+	require.True(t, user.PasswordChangedAt.Time.IsZero())
+
+	return user
+}
+
+func TestUserCreate(t *testing.T) {
+	createRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	user1 := createRandomUser(t)
+	user2, err := testQueries.GetUser(context.Background(), user1.Username)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
+	require.Equal(t, user1.FullName, user2.FullName)
+	require.Equal(t, user1.Email, user2.Email)
+
+	require.WithinDuration(t, user1.CreatedAt.Time, user2.CreatedAt.Time, time.Second)
+	require.WithinDuration(t, user1.PasswordChangedAt.Time, user2.PasswordChangedAt.Time, time.Second)
 }
